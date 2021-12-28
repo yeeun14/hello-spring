edf63557525377b8630ef140bc20153fe3141adb
yeeun14 <yeaeen0214@naver.com>
2021-12-28 18:00:35 +0900

    1. 스프링부트 테스트 코드작성

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration // Controller는 이미 Spring이 관리하기 때문에 따로 설정하지 않아도 된다.
 public class SpringConfig {
+                                                                              private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+    //    return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
